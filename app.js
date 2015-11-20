a49b13d8f9f6325fce6273a3720f6a7e2911b5ed
Aaron Zakem <Aaron Zakem>
2015-11-20 17:12:20 -0500

    add mySQL endpoint/database info

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 //Setup web server and socket
 var NYT = require('nyt');
+var mysql = require('mysql');
 
 var keys = {
     'most-popular': 'cb76354cff5a72e93e1e76afa2e4015f:15:72699233'
 }
 
 var nyt = new NYT(keys);
+
+var conn = mysql.createConnection({
+  host     : 'ajz2120stories.cz6woaizkeyb.us-west-2.rds.amazonaws.com:3306',
+  port     : '3306',
+  user     : 'topstory',
+  password : 'topstory',
+  database : 'stories',
+});
+
+conn.connect();
 
 nyt.mostPopular.viewed({'section': 'all-sections', 'time-period': '1'}, console.log);
