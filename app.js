3f489154e53fcc1582b0a9941801b4750e979cf2
Aaron Zakem <Aaron Zakem>
2015-12-17 20:14:51 -0500

    Added pull from Alchemy News and storage of top subjects for each story.

--- app.js
+++ app.js
@@ -13,14 +13,16 @@ var nyt = new NYT(keys);
 var inserted = 0;
 var insertedSubjects = 0;
 var subjects = {};
+var articleSubjects = {};
 var alchemyArticles = null;
 
 var main = function(callback) {
     nyt.mostPopular.viewed({'section': 'all-sections', 'time-period': '1'}, getGuardianArticles);
-    callback(null);
+    var delay = setTimeout(function () {callback(null);}, 15000);
 };
 
 var getGuardianArticles = function(data) {
+    console.log(JSON.parse(data).results);
     console.log('get guardian articles');
     guardian.config({
         apiKey: '5c0868ca-c973-4d1d-8062-47ed79ef96ae'
@@ -62,7 +64,7 @@ var createDatabase = function(err, gdata, ndata, callback) {
         }
     });
     // Create new TopStories table
-    conn.query("CREATE TABLE TopStories (ID int, URL VARCHAR(200), Section VARCHAR(200), Title VARCHAR(200), Abstract VARCHAR(200), Source VARCHAR(20), FirstImage VARCHAR(200), Keywords VARCHAR(200))", function(err, result) {
+    conn.query("CREATE TABLE TopStories (ID int, URL VARCHAR(200), Section VARCHAR(200), Title VARCHAR(200), Abstract VARCHAR(200), Source VARCHAR(20), FirstImage VARCHAR(200), Keywords VARCHAR(200), Subjects VARCHAR(200))", function(err, result) {
         // Catch error in creating table
         if(err) {
             console.log(err);
@@ -75,7 +77,6 @@ var createDatabase = function(err, gdata, ndata, callback) {
     });
 };
 
-
 var parseNytSubjects = function(err, jsonData, guardianData, conn, callback) {
     var arrayLength = jsonData.results.length;
     for (var i = 0; i < arrayLength; i++) {
@@ -84,10 +85,15 @@ var parseNytSubjects = function(err, jsonData, guardianData, conn, callback) {
             if (err) {
                 throw err;
             }
-            //console.log(response);
+            console.log(response);
+            var storyURL = response.url.slice(0,-5);
             var concepts = response.concepts;
-            for (var i = 0; i < 3 && i < concepts.length; i++) {
-                var subject = concepts[i].text;
+            var topArticleSubjects = [];
+            var numsubjects = concepts.length;
+            var endpoint = Math.min(numsubjects, 3);
+            for (var j = 0; j < endpoint; j++) {
+                var subject = concepts[j].text;
+                topArticleSubjects.push(subject);
                 /*if (subjects.hasOwnProperty(subject) && subject !== '') {
                   subjects[subject]++;
                   } else if (subject !== '') {
@@ -98,12 +104,20 @@ var parseNytSubjects = function(err, jsonData, guardianData, conn, callback) {
                 } else if (subject !== '') {
                     subjects[subject]++;
                 }
+                if (j === endpoint - 1) {
+                    articleSubjects[storyURL] = topArticleSubjects;
+                    console.log("Article URL:",storyURL);
+                    console.log("Article subjects:",topArticleSubjects);
+                    console.log("Array entry:",articleSubjects[storyURL]);
+                }
             }
             //console.log(subjects);
         });
+        if (i === arrayLength - 1) {
+            console.log(subjects);
+        }
     }
-    console.log(subjects);
-    callback(null, jsonData, guardianData, conn, getAlchemyArticles);
+    var delay = setTimeout(function () {callback(null, jsonData, guardianData, conn, getAlchemyArticles);},5000);
 };
 
 var parseGuardianSubjects = function(err, nytData, jsonData, conn, callback) {
@@ -115,26 +129,41 @@ var parseGuardianSubjects = function(err, nytData, jsonData, conn, callback) {
             if (err) {
                 throw err;
             }
-
+            console.log(response);
+            var storyURL = response.url;
             var concepts = response.concepts;
-            for (var i = 0; i < 3 && i < concepts.length; i++) {
-                var subject = concepts[i].text;
+            var topArticleSubjects = [];
+            var numsubjects = concepts.length;
+            var endpoint = Math.min(numsubjects, 3);
+            for (var j = 0; j < endpoint; j++) {
+                var subject = concepts[j].text;
+                topArticleSubjects.push(subject);
                 if (subjects[subject] === undefined && subject !== '') {
                     subjects[subject] = 1;
                 } else if (subject !== '') {
                     subjects[subject]++;
                 }
+                if (j === endpoint-1) {
+                    articleSubjects[storyURL] = topArticleSubjects;
+                    console.log("Article URL:",storyURL);
+                    console.log("Article subjects:",topArticleSubjects);
+                    console.log("Array entry:",articleSubjects[storyURL]);
+                }
             }
             //console.log(subjects);
-        });
+        }); //end of delay statement
+
+        if (i === arrayLength - 1) {
+            console.log(subjects);
+        }
     }
-    console.log(subjects);
-    callback(null, nytData, jsonData, conn, pushToDatabase);
+    //callback(null, nytData, jsonData, conn, pushToDatabase);
+    var delay = setTimeout(function () { callback(null, nytData, jsonData, conn, parseAlchemySubjects); }, 10000);
 };
 
 var getAlchemyArticles = function(err, nytData, guardianData, conn, callback) {
     var request = require('request');
-    request('https://gateway-a.watsonplatform.net/calls/data/GetNews?apikey=38be13e0eb95fc7806ff197fbb784d33558dbe3c&outputMode=json&start=now-1d&end=now&count=10&return=enriched.url.url,enriched.url.text,enriched.url.title,enriched.url.concepts.concept.text,enriched.url.concepts.concept.relevance', 
+    request('https://gateway-a.watsonplatform.net/calls/data/GetNews?apikey=38be13e0eb95fc7806ff197fbb784d33558dbe3c&outputMode=json&start=now-1d&end=now&count=1000&return=enriched.url.url,enriched.url.title,enriched.url.concepts.concept.text,enriched.url.concepts.concept.relevance',
             function(error, response, body) {
                 if (error) {
                     console.log(error);
@@ -143,17 +172,40 @@ var getAlchemyArticles = function(err, nytData, guardianData, conn, callback) {
                     console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Alchemy Results~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
                     console.log(info);
                     for (var i = 0; i < info.result.docs.length; i++) {
-                        console.log(info.result.docs[i].source.enriched.url.concepts);
-                        console.log(info.result.docs[i].source.enriched.url.text);
                         console.log(info.result.docs[i].source.enriched.url.title);
+                        console.log(info.result.docs[i].source.enriched.url.url);
+                        console.log(info.result.docs[i].source.enriched.url.concepts);
                     }
                     alchemyArticles = info;
+                    callback(err, nytData, guardianData, conn, pushToDatabase);
                 } else {
                     console.log('no response');
                 }
             }
     );
-    callback(err, nytData, guardianData, alchemyArticles, conn, sortSubjects);
+};
+
+var parseAlchemySubjects = function(err, nytData, guardianData, conn, callback) {
+    for (var i = 0; i < alchemyArticles.result.docs.length; i++) {
+        var url = alchemyArticles.result.docs[i].source.enriched.url.url;
+        var topArticleSubjects = [];
+        var numsubjects = alchemyArticles.result.docs[i].source.enriched.url.concepts.length;
+        var endpoint = Math.min(numsubjects, 3);
+        for (var j = 0; j < endpoint; j++) {
+            var subject = alchemyArticles.result.docs[i].source.enriched.url.concepts[j].text;
+            topArticleSubjects[j] = subject;
+                if (subjects[subject] === undefined && subject !== '') {
+                    subjects[subject] = 1;
+                } else if (subject !== '') {
+                    subjects[subject]++;
+                }
+                if (j === endpoint-1) {
+                    articleSubjects[url] = topArticleSubjects;
+                }
+            }
+            //console.log(subjects);
+        }
+    var delay = setTimeout(function () { callback(err, nytData, guardianData, alchemyArticles, conn, sortSubjects); },10000);
 };
 
 var pushToDatabase = function(err, ndata, gdata, alchemyArticles, conn, callback) {
@@ -185,9 +237,19 @@ var pushToDatabase = function(err, ndata, gdata, alchemyArticles, conn, callback
             }
         }
         var keywords = ndata.results[i].adx_keywords;
+        var subjects = "";
+        var subjectList = articleSubjects[storyURL];
+        console.log("Subject List:",subjectList);
+        console.log("URL:",storyURL);
+        if (subjectList && subjectList.length > 0) {
+            for (var j = 0; j < subjectList.length; j++) {
+                subjects = subjects + subjectList[j] + ";";
+            }
+        }
+
         article = {ID: id, URL: storyURL, Section: section, Title: title,
             Abstract: abstract, Source: source, FirstImage: firstImage,
-            Keywords: keywords};
+            Keywords: keywords, Subjects: subjects};
 
         conn.query('INSERT INTO TopStories SET ?', article, function (err, result) {
             // Catch error in inserting record
@@ -201,6 +263,7 @@ var pushToDatabase = function(err, ndata, gdata, alchemyArticles, conn, callback
         });
     }
 
+    //write Guardian stories to TopStories table
     var offset = ndata.results.length;
     for (var i = 0; i < gdata.response.results.length; i++) {
         var id = i+1+offset;
@@ -208,8 +271,16 @@ var pushToDatabase = function(err, ndata, gdata, alchemyArticles, conn, callback
         var section = gdata.response.results[i].sectionName;
         var title = gdata.response.results[i].webTitle;
         var source = "Guardian";
+        var subjects = "";
+        var subjectList = articleSubjects[storyURL];
+        if (subjectList && subjectList.length > 0) {
+            for (var j = 0; j < subjectList.length; j++) {
+                subjects = subjects + subjectList[j] + ";";
+            }
+        }
+
         article = {ID: id, URL: storyURL, Section: section, Title: title,
-            Source: source};
+            Source: source, Subjects: subjects};
         conn.query('INSERT INTO TopStories SET ?', article, function (err, result) {
             // Catch error in inserting record
             if(err) {
@@ -217,14 +288,42 @@ var pushToDatabase = function(err, ndata, gdata, alchemyArticles, conn, callback
             } else {
                 inserted++;
                 console.log(inserted,"Records Inserted - Guardian");
-                if (inserted === 30) {
+            }
+        });
+    }
+
+    //write AlchemyNews stories to TopStories table
+    offset = offset + gdata.response.results.length;
+    for (var i = 0; i < alchemyArticles.result.docs.length; i++) {
+        var id = i+1+offset;
+        var storyURL = alchemyArticles.result.docs[i].source.enriched.url.url;
+        var title = alchemyArticles.result.docs[i].source.enriched.url.title;
+        var source = "AlchemyNews";
+
+        var subjects = "";
+        var subjectList = articleSubjects[storyURL];
+        if (subjectList && subjectList.length > 0) {
+            for (var j = 0; j < subjectList.length; j++) {
+                subjects = subjects + subjectList[j] + ";";
+            }
+        }
+
+        article = {ID: id, URL: storyURL, Title: title,
+            Source: source, Subjects: subjects};
+        conn.query('INSERT INTO TopStories SET ?', article, function (err, result) {
+            // Catch error in inserting record
+            if(err) {
+                console.log(err);
+            } else {
+                inserted++;
+                console.log(inserted,"Records Inserted - AlchemyNews");
+                if (i === alchemyArticles.result.docs.length - 1 || inserted === 40) {
                     callback(conn);
                 }
             }
         });
     }
 };
-
 
 var sortSubjects = function(conn) {
     console.log(subjects);
@@ -272,4 +371,3 @@ var sortSubjects = function(conn) {
 main(function(err) {
     console.log("Final callback");
 });
-
